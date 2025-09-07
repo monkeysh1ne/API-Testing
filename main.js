be8deea4b02d90d04fd4e8bf2fc3c6c25db0cc65
monkeysh1ne <mcdoob@gmail.com>
2025-09-07 18:32:18 +1000

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+async function getItems() {
+    fetch('results.json')
+    .then((res) => res.json())
+    .then((data) => {
+        let output = '<h2>Output</h2>';
+        data.forEach(function(item){
+            output += `
+            <div id='displayData'>
+                <p>ID: ${item.id}</p>
+                <p>Title: ${item.title}</p>
+                <p>Src: <a href="${item.sourceUrl}"> ${item.sourceUrl}</a></p>
+                <p>Summary: ${item.summary}</p>
+                <h3>Ingredients ...</h3>
+            </div>
+                `;
+            if (item.extendedIngredients && Array.isArray(item.extendedIngredients)) {
+                item.extendedIngredients.forEach(function(ingredient){
+                    output += `
+                    <div id='displayIngredients'>
+                        <ul>
+                            <li>&#8226; ${ingredient.amount} ${ingredient.unit} ${ingredient.originalName}</li>
+                        </ul>
+                    </div>
+                    `;
+                });
+        };
+        document.getElementById('output').innerHTML = output;
+    });
+})
+}
+
+getItems();
