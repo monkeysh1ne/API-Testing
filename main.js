4ec1fd07e6da9dd433630c099f56a38b12ed72c7
monkeysh1ne <mcdoob@gmail.com>
2025-09-08 17:45:55 +1000

    various exercises in looping through arrays and objects

--- main.js
+++ main.js
@@ -3,32 +3,40 @@ async function getItems() {
     .then((res) => res.json())
     .then((data) => {
         let output = '<h2>Output</h2>';
-        data.forEach(function(item){
-            output += `
-            <div id='displayData'>
-                <p>ID: ${item.id}</p>
-                <p>Title: ${item.title}</p>
-                <p>Src: <a href="${item.sourceUrl}"> ${item.sourceUrl}</a></p>
-                <p>Summary: ${item.summary}</p>
-                <h3>Ingredients ...</h3>
-            </div>
-                `;
-            if (item.extendedIngredients && Array.isArray(item.extendedIngredients)) {
-                item.extendedIngredients.forEach(function(ingredient){
-                output += `
-                <div id='displayIngredients'>
-                    <ul>
-                        <li>&#8226; ${ingredient.amount} ${ingredient.unit} ${ingredient.originalName}</li>
-                    </ul>
-                </div>
-                `;
-            });
-
-            
+        const arr1 = (Object.keys(data));
+        const arr2 = (Object.values(data));
+/*         console.log(arr1);
+        console.log(arr2);
+        // display all values of results top level array.
+        for(let i=0, j=0; i<arr1.length; i++, j++){
+            console.log(i, arr1[i]);
+            console.log(j, arr2[j]);
+            // arr2[27] is nutrients
+            console.log(arr2[27].caloricBreakdown);
         };
-        document.getElementById('output').innerHTML = output;
+        for(i in arr2){
+            console.log(i,arr2[i]);
+        }; */
+/*         arr1.forEach((item, index, array)=>{
+            console.log(item);
+            console.log(index);
+            console.log(array);
+        }); */
+/*         Object.values(data).forEach((val)=> {
+            console.log(val);
+        });
+        Object.entries(data).forEach((val)=>{
+            console.log(val[0], val[1]);
+        }); */
+        // data.extendedIngredients.forEach(el => console.log(el));
+        // data.nutrition.nutrients.forEach(el => console.log(el));
+        data.nutrition.nutrients.forEach(nutrient => {
+            console.log(nutrient);
+            output += `
+            <p>${nutrient}</p>
+            `;
+        });
     });
-})
 }
 
 getItems();
